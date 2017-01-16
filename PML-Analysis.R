cb32a053b1a04df1d5656a079aebb78bdc0a9c5a
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-01-15 20:44:11 -0500

    Fix a typo

--- PML-Analysis.R
+++ PML-Analysis.R
@@ -20,10 +20,10 @@ na.values <- c('NA','#DIV/0!', '')
 training <- read.csv('pml-training.csv', na.strings = na.values)
 testing <- read.csv('pml-testing.csv', na.strings = na.values)
 
-# Strip summary columns that are primarily NAs (and 
-# don't provide useful values for each attempt anyway since they're summaries) 
+# Strip summary columns that are primarily NAs (and
+# don't provide useful values for each attempt anyway since they're summaries)
 # for both testing and training sets.
- 
+
 # Also remove the category columns
 training_clean <- training[,(colSums(is.na(training)) == 0)]
 training_clean <- training_clean[,-(1:7)]
@@ -36,14 +36,14 @@ testing_clean <- testing_clean[,-(1:7)]
 set.seed(4475)
 
 
-# Split training set 60/40 to make a cross-validation set to establish 
-# the expectedrate of error.
+# Split training set 60/40 to make a cross-validation set to establish
+# the expected rate of error.
 
 inTrain <-createDataPartition(y=training_clean$classe, p = 0.6, list=FALSE)
 training <- training_clean[inTrain,]
 validation <- training_clean[-inTrain,]
 
-# Do a quick decision tree using rpart, just to get a sense of how effective 
+# Do a quick decision tree using rpart, just to get a sense of how effective
 # a low accuracy sort might be on the dataset as is.
 rpart_fit <- train(classe ~ ., data=training, method = 'rpart')
 pred1 <- predict(rpart_fit, validation)
@@ -56,9 +56,9 @@ dev.off()
 # Get the out of sample error for the cross-validation data
 confusionMatrix(pred1, validation$classe)
 
-# Use a bagged CAR for accuracy of a multi-variate
-# data set but efficiency of processing, 
-# since the computationally easy method produced < 50% out-of-sample in the 
+# Use a bagged CART for accuracy of a multi-variate
+# data set but efficiency of processing,
+# since the computationally easy method produced < 50% out-of-sample in the
 # reserved cross-validation data.
 bag_fit <- train(classe ~ ., data = training, method = 'treebag')
 pred2 <- predict(bag_fit, validation)
@@ -77,6 +77,3 @@ confusionMatrix(pred2, validation$classe)
 
 # Print final values against 20 external samples
 predict(bag_fit, testing_clean)
-
-
-
